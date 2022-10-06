138dbebc194a832e489c8c098810621834485d42
Mitch3207 <9018785@student.zadkine.nl>
2022-10-06 13:43:29 +0200

    je vader

---
+++ 5.1 javascript/app.js
@@ -0,0 +1,2 @@
+document.getElementById("h1").style.color = "purple";
+document.getElementById("h1").innerText = "ik ben klaar"
---
+++ DOM Counter/app.js
@@ -0,0 +1,19 @@
+let title = document.getElementById("title");   
+let btn = document.getElementById("btn");
+let btn2 = document.getElementById("btn2");
+
+
+let count = 0;
+
+function counter(){
+    count += 1;
+    title.innerText = count + " keer geklikt";
+    document.body.style.backgroundColor = "blue";
+    btn.style.backgroundColor = "red"
+}
+function counter2() {
+    count -= 1;
+    title.innerText = count + " keer geklikt";
+    document.body.style.backgroundColor = "red";
+    btn2.style.backgroundColor = "blue";
+}
