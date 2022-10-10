9c846a1bf8e22105fa5c9ff6df9662c599c324d3
Mitch3207 <9018785@student.zadkine.nl>
2022-10-10 13:29:22 +0200

    java

---
+++ 6.4/app.js
@@ -0,0 +1,25 @@
+function calculate(x, y) {
+    let answer = x + y;
+    console.log(answer);
+}
+
+calculator(1, 12); 
+
+function multiplyByFive(x) {
+    let answer = x * 5;
+    console.log(answer);
+}
+
+multiplyByFive(7);
+
+function calculateMinutes(x) {
+console.log(x / 60);
+}
+
+calculateMinutes(3600);
+
+
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+
+console.log(input1);
