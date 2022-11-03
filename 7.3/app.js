a0bcd8c1085d421cc102e601bdfc6cef4c1e3453
Mitch3207 <9018785@student.zadkine.nl>
2022-11-03 14:13:38 +0100

    javascript

---
+++ 7.3/app.js
@@ -0,0 +1,19 @@
+let title = document.getElementById("title");   
+let btn = document.getElementById("btn");
+
+
+let count = 0;
+
+
+
+function counter(){
+    if(count >= 10){
+        count = 0
+    } else{
+        count+=1
+    }
+  
+    title.innerText = count + " keer geklikt";
+}
+
+
