0bdd2366942de352f47f024e1434b251b3116ae1
Mitch3207 <9018785@student.zadkine.nl>
2022-11-03 13:37:55 +0100

    javascript 7.2

--- 7.1/app.js
+++ 7.1/app.js
@@ -1,13 +1,13 @@
 let budget = 100;
 
-let product = 180;
+let product = prompt();
 
 let h1 = document.getElementById("uitkomst");
 
-if(budget > product){
+if (budget > product){
     h1.innerText = "ik heb genoeg geld";
     h1.style.color = "green";
-}else {
+} else {
     h1.innerText = "ik heb te weinig geld"
     h1.style.color = "red";
 }
